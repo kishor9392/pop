e086f5a08f9f591c62ea12fb32596463aa6c5757
maddineni nandakishore <115626959+kishor9392@users.noreply.github.com>
2023-12-10 18:46:18 +0530

    Update index.js

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,12 +1,22 @@
+import Header from '../Header'
+
 import './index.css'
 
 const Home = () => (
-  <div className="aboutBg">
-    <img
-      src="https://assets.ccbp.in/frontend/react-js/home-lg-img.png"
-      alt="home"
-      className="aboutIcon"
-    />
+  <div className="home-container">
+    <Header />
+    <div className="home-image-container">
+      <img
+        className="mobile-home-image"
+        src="https://assets.ccbp.in/frontend/react-js/home-sm-img.png"
+        alt="home"
+      />
+      <img
+        className="desktop-home-image"
+        src="https://assets.ccbp.in/frontend/react-js/home-lg-img.png"
+        alt="home"
+      />
+    </div>
   </div>
 )
 
