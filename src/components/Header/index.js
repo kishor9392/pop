b5e3e5f6568f54291b19afa713b1c5908247341f
maddineni nandakishore <115626959+kishor9392@users.noreply.github.com>
2023-12-10 18:45:30 +0530

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,83 +1,64 @@
 import Popup from 'reactjs-popup'
-import {Link} from 'react-router-dom'
-
-import 'reactjs-popup/dist/index.css'
-
+import {withRouter, Link} from 'react-router-dom'
 import {GiHamburgerMenu} from 'react-icons/gi'
+import {IoMdClose} from 'react-icons/io'
 import {AiFillHome} from 'react-icons/ai'
 import {BsInfoCircleFill} from 'react-icons/bs'
-import {IoMdClose} from 'react-icons/io'
 
 import './index.css'
 
-const overlayStyles = {
-  backgroundColor: '#ffff',
-}
-
 const Header = () => (
-  <div className="HeadBg">
-    <Link to="/">
-      <img
-        src="https://assets.ccbp.in/frontend/react-js/hamburger-menu-website-logo.png"
-        alt="website logo"
-        className="headImg"
-      />
-    </Link>
-
-    <Popup
-      modal
-      trigger={
-        <button type="button" className="btn" data-testid="hamburgerIconButton">
-          <>
-            <GiHamburgerMenu className="menu" />
-            <p>''</p>
-          </>
-        </button>
-      }
-      overlayStyle={overlayStyles}
-    >
-      {close => (
-        <>
-          <ul className="mg">
-            <div className="closeBg">
-              <Link to="/">
-                <li>
-                  <button
-                    type="button"
-                    className="btn"
-                    data-testid="hamburgerIconButton"
-                  >
-                    <AiFillHome className="icon" />
-                    <h1>Home</h1>
-                  </button>
-                </li>
-              </Link>
-
-              <Link to="/about">
-                <li>
-                  <button
-                    type="button"
-                    className="btn"
-                    data-testid="hamburgerIconButton"
-                  >
-                    <BsInfoCircleFill className="icon" />
-                    <h1>About</h1>
-                  </button>
-                </li>
-              </Link>
-            </div>
-
-            <button type="button" className="btn" data-testid="closeButton">
-              <>
-                <IoMdClose className="close" onClick={() => close()} />
-                <p>''</p>
-              </>
+  <div className="nav-header">
+    <div className="nav-content">
+      <Link to="/">
+        <img
+          className="website-logo"
+          src="https://assets.ccbp.in/frontend/react-js/hamburger-menu-website-logo.png"
+          alt="website logo"
+        />
+      </Link>
+      <Popup
+        modal
+        trigger={
+          <button
+            className="hamburger-icon-button"
+            type="button"
+            data-testid="hamburgerIconButton"
+          >
+            <GiHamburgerMenu size="30" />
+          </button>
+        }
+        className="popup-content"
+      >
+        {close => (
+          <div className="modal-container">
+            <button
+              className="close-button"
+              type="button"
+              data-testid="closeButton"
+              onClick={() => close()}
+            >
+              <IoMdClose size="30" color="#616e7c" />
             </button>
-          </ul>
-        </>
-      )}
-    </Popup>
+            <ul className="nav-links-list">
+              <li className="nav-link-item">
+                <Link className="nav-link" to="/" onClick={() => close()}>
+                  <AiFillHome size="36" />
+                  <p className="nav-link-content">Home</p>
+                </Link>
+              </li>
+              <li className="nav-link-item">
+                <Link className="nav-link" to="/about" onClick={() => close()}>
+                  <BsInfoCircleFill size="32" />
+                  <p className="nav-link-content">About</p>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        )}
+      </Popup>
+    </div>
   </div>
 )
 
-export default Header
+export default withRouter(Header)
