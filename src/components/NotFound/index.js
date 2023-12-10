55eeca0884bb109143f633776e255310584a7a48
maddineni nandakishore <115626959+kishor9392@users.noreply.github.com>
2023-12-10 18:47:04 +0530

    Update index.js

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,17 +1,24 @@
+import Header from '../Header'
+
 import './index.css'
 
 const NotFound = () => (
-  <div className="notBg">
-    <img
-      src="https://assets.ccbp.in/frontend/react-js/not-found-img.png"
-      alt="not found"
-      className="notImg"
-    />
-    <h1 className="notH1">Lost Your Way?</h1>
-    <p className="notP">
-      Sorry, we cannot find that page. You will find lots to explore on the home
-      page
-    </p>
+  <div className="not-found-container">
+    <Header />
+    <div className="not-found-content-container">
+      <div className="not-found-responsive-container">
+        <img
+          className="image"
+          src="https://assets.ccbp.in/frontend/react-js/not-found-img.png"
+          alt="not found"
+        />
+        <h1 className="heading">Lost Your Way?</h1>
+        <p className="description">
+          Sorry, we cannot find that page. You will find lots to explore on the
+          home page
+        </p>
+      </div>
+    </div>
   </div>
 )
 
